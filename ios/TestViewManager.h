74392fe8e69536a05ee776e00fff446c0afebb21
nipeiqing <nipeiqing@avocadocondo.com>
2020-04-10 17:35:54 +0800

    merge loacl

---
+++ ios/TestViewManager.h
@@ -0,0 +1,17 @@
+//
+//  TestViewManager.h
+//  BridgingProject
+//
+//  Created by mac on 2020/4/2.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestViewManager : RCTViewManager
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/TestViewManager.m
@@ -0,0 +1,13 @@
+//
+//  TestViewManager.m
+//  BridgingProject
+//
+//  Created by mac on 2020/4/2.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#import "TestViewManager.h"
+
+@implementation TestViewManager
+
+@end
